77b800bbc7e62230a99e660792ffc85c7fb6bf19
joao paulo <pool.martin@gmail.com>
2018-10-14 15:18:46 -0300

    fix on flags split_type

--- utils/flags.py
+++ utils/flags.py
@@ -172,9 +172,9 @@ def define_flags():
     tf.app.flags.DEFINE_integer(
         'image_channels', 3, 'channels of the entry images.')
 
-    if(tf.app.flags.FLAGS.model_name in ['i3d']):
+    if(tf.app.flags.FLAGS.model_name in ['i3d', 'c3d']):
         tf.app.flags.FLAGS.split_type = '3D'
-    elif(tf.app.flags.FLAGS.model_name in ['inception_v4']):
+    elif(tf.app.flags.FLAGS.model_name in ['inception_v1', 'inception_v4']):
         tf.app.flags.FLAGS.split_type = '2D'
 
 
