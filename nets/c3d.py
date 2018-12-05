1889e20b2342b3b2ed18372789db7a59efa1a544
joao paulo <pool.martin@gmail.com>
2018-12-05 21:19:47 -0200

    c3d without tf.slim

--- nets/c3d.py
+++ nets/c3d.py
@@ -1,40 +1,54 @@
 import tensorflow as tf
-import tensorflow.contrib.slim as slim
+# import tensorflow.contrib.slim as slim
 
 def C3D(input, num_classes, keep_pro=0.2):
     end_points = {}
     with tf.variable_scope('C3D'):
-        with slim.arg_scope([slim.conv3d],
-                            padding='SAME',
-                            weights_regularizer=slim.l2_regularizer(0.0005),
-                            activation_fn=tf.nn.relu,
-                            kernel_size=[3, 3, 3],
-                            stride=[1, 1, 1]
-                            ):
-            net = slim.conv3d(input, 64, scope='conv1')
+        # with slim.arg_scope([slim.conv3d],
+        #                     padding='SAME',
+        #                     weights_regularizer=slim.l2_regularizer(0.0005),
+        #                     activation_fn=tf.nn.relu,
+        #                     kernel_size=[3, 3, 3],
+        #                     stride=[1, 1, 1]
+        #                     ):
+            net = tf.layers.conv3d(input, 64, kernel_size=[3, 3, 3], strides=[1, 1, 1], padding='SAME', activation=tf.nn.relu)
+            # net = slim.conv3d(input, 64, scope='conv1')
             end_points['conv1'] = net
-            net = slim.max_pool3d(net, kernel_size=[1, 2, 2], stride=[1, 2, 2], padding='VALID', scope='max_pool1')
+            net = tf.layers.max_pooling3d(net, pool_size=[1, 2, 2], strides=[ 1, 2, 2], padding='VALID')
+            # net = slim.max_pool3d(net, kernel_size=[1, 2, 2], stride=[1, 2, 2], padding='VALID', scope='max_pool1')
             end_points['max_pool1'] = net
-            net = slim.conv3d(net, 128, scope='conv2')
+            net = tf.layers.conv3d(net, 128, kernel_size=[3, 3, 3], strides=[1, 1, 1], padding='SAME', activation=tf.nn.relu)
+            # net = slim.conv3d(net, 128, scope='conv2')
             end_points['conv2'] = net
-            net = slim.max_pool3d(net, kernel_size=[2, 2, 2], stride=[2, 2, 2], padding='VALID', scope='max_pool2')
+            net = tf.layers.max_pooling3d(net, pool_size=[2, 2, 2], strides=[ 2, 2, 2], padding='VALID')
+            # net = slim.max_pool3d(net, kernel_size=[2, 2, 2], stride=[2, 2, 2], padding='VALID', scope='max_pool2')
             end_points['max_pool2'] = net
-            net = slim.repeat(net, 2, slim.conv3d, 256, scope='conv3')
+            net = tf.layers.conv3d(net, 256, kernel_size=[3, 3, 3], strides=[1, 1, 1], padding='SAME', activation=tf.nn.relu)
+            net = tf.layers.conv3d(net, 256, kernel_size=[3, 3, 3], strides=[1, 1, 1], padding='SAME', activation=tf.nn.relu)
+            # net = slim.repeat(net, 2, slim.conv3d, 256, scope='conv3')
             end_points['conv3'] = net
-            net = slim.max_pool3d(net, kernel_size=[2, 2, 2], stride=[2, 2, 2], padding='VALID', scope='max_pool3')
+            net = tf.layers.max_pooling3d(net, pool_size=[2, 2, 2], strides=[ 2, 2, 2], padding='VALID')
+            # net = slim.max_pool3d(net, kernel_size=[2, 2, 2], stride=[2, 2, 2], padding='VALID', scope='max_pool3')
             end_points['max_pool3'] = net
             # net = slim.repeat(net, 2, slim.conv3d, 512, scope='conv4')
-            net = slim.conv3d(net, 512, scope='conv4a')
+            net = tf.layers.conv3d(net, 512, kernel_size=[3, 3, 3], strides=[1, 1, 1], padding='SAME', activation=tf.nn.relu)
+            # net = slim.conv3d(net, 512, scope='conv4a')
+            end_points['conv4a'] = net
             # net = tf.nn.batch_normalization(net)
-            net = slim.conv3d(net, 512, scope='conv4b')
+            net = tf.layers.conv3d(net, 512, kernel_size=[3, 3, 3], strides=[1, 1, 1], padding='SAME', activation=tf.nn.relu)
+            # net = slim.conv3d(net, 512, scope='conv4b')
             end_points['conv4b'] = net
-            net = slim.max_pool3d(net, kernel_size=[2, 2, 2], stride=[2, 2, 2], padding='VALID', scope='max_pool4')
+            net = tf.layers.max_pooling3d(net, pool_size=[2, 2, 2], strides=[ 2, 2, 2], padding='VALID')
+            # net = slim.max_pool3d(net, kernel_size=[2, 2, 2], stride=[2, 2, 2], padding='VALID', scope='max_pool4')
             end_points['max_pool4'] = net
-            net = slim.repeat(net, 2, slim.conv3d, 512, scope='conv5')
+            net = tf.layers.conv3d(net, 512, kernel_size=[3, 3, 3], strides=[1, 1, 1], padding='SAME', activation=tf.nn.relu)
+            net = tf.layers.conv3d(net, 512, kernel_size=[3, 3, 3], strides=[1, 1, 1], padding='SAME', activation=tf.nn.relu)
+            # net = slim.repeat(net, 2, slim.conv3d, 512, scope='conv5')
             end_points['conv5'] = net
             net = tf.pad(net, paddings=[[0,0], [0,0], [1,0], [1,0], [0,0]])
             end_points['padding'] = net
-            net = slim.max_pool3d(net, kernel_size=[1, 2, 2], stride=[1, 3, 3], padding='VALID', scope='max_pool5')
+            net = tf.layers.max_pooling3d(net, pool_size=[1, 2, 2], strides=[ 1, 3, 3], padding='VALID')
+            # net = slim.max_pool3d(net, kernel_size=[1, 2, 2], stride=[1, 3, 3], padding='VALID', scope='max_pool5')
             end_points['max_pool5'] = net
 
             # net = tf.reshape(net, [-1, 512 * 7 * 7])
@@ -43,11 +57,13 @@ def C3D(input, num_classes, keep_pro=0.2):
             net = tf.layers.dense(net, 4096, activation=tf.nn.relu)
             # net =  slim.fully_connected(net, 4096, activation_fn=tf.nn.relu, scope='fc6')
             end_points['fc6'] = net
-            net = slim.dropout(net, 0.3, scope='dropout1')
+            net = tf.layers.dropout(net, rate=0.3)
+            # net = slim.dropout(net, 0.3, scope='dropout1')
             # net = slim.fully_connected(net, 4096, weights_regularizer=slim.l2_regularizer(0.0005), scope='fc7')
             net = tf.layers.dense(net, 4096, activation=tf.nn.relu)
             end_points['fc7'] = net
-            net = slim.dropout(net, keep_pro, scope='dropout2')
+            net = tf.layers.dropout(net, rate=keep_pro)
+            # net = slim.dropout(net, keep_pro, scope='dropout2')
             # out = slim.fully_connected(net, num_classes, weights_regularizer=slim.l2_regularizer(0.0005), activation_fn=None, scope='out')
             out = tf.layers.dense(net, num_classes, activation=None)
             end_points['Logits'] = out
